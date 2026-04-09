0c096ced1e3b42fbaa216877eb2a39adf60feba3
Eliza1213 <20230015@uthh.edu.mx>
2026-04-09 10:34:35 -0600

    feat: Server

--- backend/server.js
+++ backend/server.js
@@ -146,6 +146,6 @@ app.use('/api/admin/reabastecimiento', reabastecimientoRoutes);
    INICIAR SERVIDOR
 ================================ */
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
